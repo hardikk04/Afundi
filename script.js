94ffcdcf5f6a35f76d59c297e744a7e1310d6913
hardikk04 <hardik04062003@gmail.com>
2023-11-21 18:36:58 +0530

    page1 half cmplt

---
+++ script.js
@@ -0,0 +1,51 @@
+const brandImg1 = document.querySelector(".brand1");
+const brandImg2 = document.querySelector(".brand2");
+const brandImg3 = document.querySelector(".brand3");
+const brandTag = document.querySelector(".page1-heading-top > h1>span");
+console.log(brandTag);
+
+brandTag.addEventListener("mouseenter", () => {
+  brandImg1.style.scale = 1;
+  brandImg2.style.scale = 1;
+  brandImg3.style.scale = 1;
+});
+
+brandTag.addEventListener("mouseleave", () => {
+  brandImg1.style.scale = 0;
+  brandImg2.style.scale = 0;
+  brandImg3.style.scale = 0;
+});
+
+const websiteImg1 = document.querySelector(".website1");
+const websiteImg2 = document.querySelector(".website2");
+const websiteImg3 = document.querySelector(".website3");
+const websiteTag = document.querySelector(".page1-heading-center > h1>span");
+
+websiteTag.addEventListener("mouseenter", () => {
+  websiteImg1.style.scale = 1;
+  websiteImg2.style.scale = 1;
+  websiteImg3.style.scale = 1;
+});
+
+websiteTag.addEventListener("mouseleave", () => {
+  websiteImg1.style.scale = 0;
+  websiteImg2.style.scale = 0;
+  websiteImg3.style.scale = 0;
+});
+
+const plateformImg1 = document.querySelector(".plateform1");
+const plateformImg2 = document.querySelector(".plateform2");
+const plateformImg3 = document.querySelector(".plateform3");
+const plateformTag = document.querySelector(".page1-heading-bottom > h1>span");
+
+plateformTag.addEventListener("mouseenter", () => {
+  plateformImg1.style.scale = 1;
+  plateformImg2.style.scale = 1;
+  plateformImg3.style.scale = 1;
+});
+
+plateformTag.addEventListener("mouseleave", () => {
+  plateformImg1.style.scale = 0;
+  plateformImg2.style.scale = 0;
+  plateformImg3.style.scale = 0;
+});
